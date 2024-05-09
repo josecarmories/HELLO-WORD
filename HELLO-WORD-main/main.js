2995e3f8bcccfe11492bd8950462e64730c72bc3
josecarmoreis <159275153+josecarmories@users.noreply.github.com>
2024-05-09 09:40:24 -0300

    Add files via upload
    Construção main.js

---
+++ HELLO-WORD-main/main.js
@@ -0,0 +1,13 @@
+let nomeUsuário = "";
+let elemento = document.querySelector("#nome-usuário"); 
+
+wile(nomeUsuário ==){
+    nomeUsuário = prompt("Qual o seu nome?"); 
+}
+
+if(nomeUsuário == null){
+    elemento.textContent = "seja muito ben-vind0.";
+}else{
+    elemento. textContent = nomeUsuário; 
+}
+
